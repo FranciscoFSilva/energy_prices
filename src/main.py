58a315479d35213ea18fcf8adc56fa4d84966b56
Francisco F. SIlva <22647533+FranciscoFSilva@users.noreply.github.com>
2024-11-30 13:35:25 +0000

    url response printed

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 import datetime_utils
 import request
 import url_utils
+from errors import ContentTypeIsNotPlainTextError
 
 
 def main() -> None:
     tommorows_date = datetime_utils.get_tomorrows_datetime()
     url = url_utils.get_price_url(tommorows_date)
     r = request.request_omie_energy_prices(url)
-    print(r)
+
+    if request.check_content_type(r) != "text/plain":
+        raise ContentTypeIsNotPlainTextError("Check if omie.es changed the file type")
+
+    response = request.get_request_content(r)
+
+    print(response)
 
 
 if __name__ == "__main__":
